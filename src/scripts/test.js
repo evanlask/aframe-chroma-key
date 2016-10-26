7029a5a22c775c938d2e2aa037c8d611ccfac156
Evan Lask <evanlask@gmail.com>
2016-10-26 17:03:35 -0600

    Some more tweaks.

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -8,15 +8,26 @@ video.addEventListener('play', function () {
     ctx.drawImage(video, 0, 0, video.width, video.height);
 
     // Chroma key
-    var tolerance = 150;
+
     var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     var data = imageData.data;
+
+    // FULL TRANSPARENT
+    //var tolerance = 150;
+    //for(var i = 0, n = data.length; i < n; i += 4) {
+    //  var diff = Math.abs(data[i] - data[0]) + Math.abs(data[i+1] - data[1]) + Math.abs(data[i+2] - data[2]);
+    //  if(diff < tolerance) {
+    //    data[i + 3] = 0;
+    //  }
+    //}
+
+    // SEMI TRANSPARENT
+    var tolerance = 150;
     for(var i = 0, n = data.length; i < n; i += 4) {
       var diff = Math.abs(data[i] - data[0]) + Math.abs(data[i+1] - data[1]) + Math.abs(data[i+2] - data[2]);
-      if(diff < tolerance) {
-        data[i + 3] = 0;
-      }
+      data[i + 3] = (diff*diff)/tolerance;
     }
+
     ctx.putImageData(imageData, 0, 0);
   });
 }, null);
