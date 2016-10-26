5be535f30991896f126aa9f8c2f1579cb75b6734
Evan Lask <evanlask@gmail.com>
2016-10-26 15:14:09 -0600

    Chroma key working good enough.

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,2 +1,22 @@
-var root = 'ROOT';
-console.log(root);
+var canvas = document.getElementById('video-canvas');
+var ctx = canvas.getContext('2d');
+var video = document.getElementById('video-element');
+
+video.addEventListener('play', function () {
+  setInterval(function() {
+    // Write video frame to canvas
+    ctx.drawImage(video, 0, 0, video.width, video.height);
+
+    // Chroma key
+    var tolerance = 150;
+    var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+    for(var i = 0, n = data.length; i < n; i += 4) {
+      var diff = Math.abs(data[i] - data[0]) + Math.abs(data[i+1] - data[1]) + Math.abs(data[i+2] - data[2]);
+      if(diff < tolerance) {
+        data[i + 3] = 0;
+      }
+    }
+    ctx.putImageData(imageData, 0, 0);
+  });
+}, null);
